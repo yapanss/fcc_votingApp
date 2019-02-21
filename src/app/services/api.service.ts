bac6ad5ab354a5dabe83e094e1dba8234e24fb6a
yapanss <yapanss@yahoo.fr>
2019-02-21 13:58:53 +0000

    try to fix api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,10 +7,12 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
+  const baseUrl = "/api/"
+
   constructor(private http: HttpClient) { }
 
   postForm(collection, data) {
-    let url = 'http://localhost:3000/api/'+collection
+    let url = baseUrl+collection
     let body = JSON.stringify(data)
     return this.http.post(url, body, {
       headers: new HttpHeaders({
@@ -20,22 +22,22 @@ export class ApiService {
   }
 
   getAllList(collection) {
-    let url = 'http://localhost:3000/api/'+collection
+    let url = baseUrl+collection
     return this.http.get(url)
   }
 
   getSomeList(collection, parame) {
-    let url = 'http://localhost:3000/api/'+collection+'/'+parame
+    let url = baseUrl+collection+'/'+parame
     return this.http.get(url)
   }
 
   getItem(collection, id) {
-    let url = 'http://localhost:3000/api/'+collection+'/'+id
+    let url = baseUrl+collection+'/'+id
     return this.http.get(url)
   }
 
    updateItem(collection, id, data) {
-    let url = 'http://localhost:3000/api/'+collection+'/'+id
+    let url = baseUrl+collection+'/'+id
     let body = JSON.stringify(data)
 
     return this.http.put(url, body, {
@@ -46,13 +48,13 @@ export class ApiService {
   }
 
    deleteItem(collection, id) {
-    let url = 'http://localhost:3000/api/'+collection+'/'+id
+    let url = baseUrl+collection+'/'+id
     return this.http.delete(url)
   }
 
 
   updateVote(id1, index, data) {
-    let url = 'http://localhost:3000/api/poll/'+id1+'/'+index
+    let url = baseUrl+'poll/'+id1+'/'+index
     let body = JSON.stringify(data)
     return this.http.put(url, body, {
       headers: new HttpHeaders({
@@ -61,13 +63,4 @@ export class ApiService {
     })
   }
 
-  shareOnTwitter(text) {
-    let url = 'https://twitter.com/intent/tweet?text='+text;
-    return this.http.post(url, {
-        headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': "*"
-      })
-    })
-  }
 }
