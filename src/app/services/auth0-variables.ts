07b5320d39eed2422f34e1d9a809a3235d27424f
yapanss <yapanss@yahoo.fr>
2019-02-21 09:23:32 +0000

    change Auth_Config callbackURL from localhost:4200/login to myvotingapp.herkuuapp.com/login

--- src/app/services/auth0-variables.ts
+++ src/app/services/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: '2iMS8o0JVji3WlO5kOa453Rzm47tz1KZ',
   domain: 'yapanss.auth0.com',
-  callbackURL: 'http://localhost:4200/login'
+  callbackURL: 'https://myvotingapp.herokuapp.com/login'
 };
